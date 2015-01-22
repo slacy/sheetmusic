43a043ba9747946e3400da463119524ade293d2c
Steve Lacy <slacy@google.com>
2015-01-21 16:20:42 -0800

    Add reformat script

---
+++ RyansMammothCollection/tools/reformat.sh
@@ -0,0 +1,9 @@
+perl -pi -e "s/\n/ /g" AlistairMaclalastairStrathspey.ly
+perl -pi -e "s/[^^_\.]\\\/\n\\\/g" AlistairMaclalastairStrathspey.ly
+perl -pi -e "s/(crossRefNumber|footnotes|tagline|title|voicedefault)/\n\1/g" AlistairMaclalastairStrathspey.ly
+perl -pi -e "s/\|/\|\n/g" AlistairMaclalastairStrathspey.ly
+perl -pi -e "s/{/{\n/g" AlistairMaclalastairStrathspey.ly
+emacs -batch AlistairMaclalastairStrathspey.ly --eval '(indent-region (point-min) (point-max) nil)' -f save-buffer
+
+
+perl -pi -e "s/(\\\repeat volta 2 {)/\n\1\n/g" AlistairMaclalastairStrathspey.ly
