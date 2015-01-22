8c6b84819a1315d9b494efc943a42c8a5f77b3d1
Steve Lacy <slacy@google.com>
2015-01-21 16:32:45 -0800

    Reformatter

--- RyansMammothCollection/tools/reformat.sh
+++ RyansMammothCollection/tools/reformat.sh
@@ -6,4 +6,7 @@ perl -pi -e "s/{/{\n/g" AlistairMaclalastairStrathspey.ly
 emacs -batch AlistairMaclalastairStrathspey.ly --eval '(indent-region (point-min) (point-max) nil)' -f save-buffer
 
 
-perl -pi -e "s/(\\\repeat volta 2 {)/\n\1\n/g" AlistairMaclalastairStrathspey.ly
+perl -pi -e "s/(\\\repeat volta 2 {)/\n\1\n/g" 7thRegimentReel.ly
+perl -pi -e "s/\}/\n\}/g" 7thRegimentReel.ly
+
+perl -pi -e "s/(\\\key [a-z] \\\[a-z]+)/\1\n/g" 7thRegimentReel.ly
